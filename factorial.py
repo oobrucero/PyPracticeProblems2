012f0bd6085e051e32c2571db219522a67fd41ba
Bruce Reier <34bpr54@gmail.com>
2020-08-03 17:03:40 -0700

    factorial problem

---
+++ factorial.py
@@ -0,0 +1,8 @@
+def factorial(num):
+  answer = 1
+  for x in range(1, num + 1):
+    answer *=x
+  return answer
+
+int1 = int(input("Enter the number you want to find the factorial of: "))
+print(factorial(int1))
