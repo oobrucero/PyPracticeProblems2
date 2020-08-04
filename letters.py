be1cf45b4f2279485f272703d82c721d47f178df
Bruce Reier <34bpr54@gmail.com>
2020-08-03 17:22:41 -0700

    letters problem

---
+++ letters.py
@@ -0,0 +1,9 @@
+ustring = input("Enter the string you would like to search through: ")
+char = input("Enter the chacter you would like to know the count of: ")
+if len(char) != 1:
+  print("That character is invalid")
+else:
+  upchar = char.upper()
+  lowchar = char.lower()
+  ustring = ustring.lower()
+  print("The letter " + "\"" + upchar + "\" is in your string " + str(ustring.count(lowchar)) + " times.")
