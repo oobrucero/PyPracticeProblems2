ba92b9eddbc284f3b43df8710a08f8681bdb306b
Bruce Reier <34bpr54@gmail.com>
2020-08-09 19:32:45 -0700

    list searching problem

---
+++ listsearch.py
@@ -0,0 +1,8 @@
+list1 = [6, 1, 3, 0, 2]
+num = int(input("Enter a number to search for: "))
+for x in range(0, len(list1)):
+  if list1[x] == num:
+    print("The number " + str(num) + " is in index position " + str(x) + " in the list.")
+    break
+  if x == len(list1) - 1:
+    print("The number is not in the list.")
